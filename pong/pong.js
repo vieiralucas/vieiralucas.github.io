aad9ed3968cb9c396321698ccdc928cebaf8947b
Lucas Vieira <lucas@lucas-ubuntu.(none)>
2014-06-07 12:40:04 -0300

    upando nova versao de pong

--- pong/pong.js
+++ pong/pong.js
@@ -3,12 +3,105 @@ window.onload = function() {
 	document.addEventListener("keydown", keyPressed);
 	document.addEventListener("keyup", keyReleased)
 
+	//0: menu; 1: player vs player; 2: player vs computer; 3: co-op 4: Player vs Wall;
+	var gameMode = 0;
 	var W = 800, H = 400;
-	var upArrow = 38, downArrow = 40, wKey = 87, sKey = 83;
+	var upArrow = 38, downArrow = 40, wKey = 87, sKey = 83, enterKey = 13, escKey = 27;
+	var blueScore = 0, redScore = 0, computerScore = 0, coopScore, wallScore;
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
 	canvas.width = W;
 	canvas.height = H;
+
+	var menu = {
+		selected: 0,
+		pvp: {
+			txt: "Player vs Player",
+			draw: function() {
+				var width = ctx.measureText(this.txt).width;
+				var x = W/2 - width/2;
+				var y = 150;
+				ctx.fillText(this.txt, x, y);
+				if(menu.selected === 0) {
+					ctx.fillRect(x, y+10, width, 2);
+				}
+			}			
+		},
+		pvc: {
+			txt: "Player vs Computer",
+			draw: function() {
+				var width = ctx.measureText(this.txt).width;
+				var x = W/2 - width/2;
+				var y = 210;
+				ctx.fillText(this.txt, x, y);
+				if(menu.selected === 1) {
+					ctx.fillRect(x, y+10, width, 2);
+				}
+			}
+		},
+		coop: {
+			txt: "Co-Op",
+			draw: function() {
+				var width = ctx.measureText(this.txt).width;
+				var x = W/2 - width/2;
+				var y = 270;
+				ctx.fillText(this.txt, x, y);
+				if(menu.selected === 2) {
+					ctx.fillRect(x, y+10, width, 2);
+				}
+			}
+		},
+		pvw: {
+			txt: "Player vs Wall",
+			draw: function() {
+				var width = ctx.measureText(this.txt).width;
+				var x = W/2 - width/2;
+				var y = 330;
+				ctx.fillText(this.txt, x, y);
+				if(menu.selected === 3) {
+					ctx.fillRect(x, y+10, width, 2);
+				}
+			}
+		}, 
+		font: "30px Helvetica",
+		update: function() {
+
+		},
+		draw: function() {
+			ctx.fillStyle = "#FFF";
+			ctx.font = this.font;
+			this.pvp.draw();
+			this.pvc.draw();
+			this.coop.draw();
+			this.pvw.draw();
+		},
+		selectUp: function() {
+			this.selected--;
+			if(this.selected < 0) {
+				this.selected = 3;
+			}
+		},
+		selectDown: function() {
+			this.selected++;
+			if(this.selected > 3) {
+				this.selected = 0;
+			}
+		}, 
+		chooseGameMode: function() {
+			if(this.selected === 0) {
+				startPvP();
+			} else if(this.selected === 1) {
+				this.selected = 0;
+				startPvC();
+			} else if(this.selected === 2) {
+				this.selected = 0;
+				startCoOp();
+			} else {
+				this.selected = 0;
+				startPvW();
+			}
+		}
+	};
 
 	var player = {
 		blue: new Player(20, "#00F"),
@@ -57,7 +150,7 @@ window.onload = function() {
 				this.dy = 12;
 			}
 		};
-		
+
 		this.stop = function() {
 			this.dy = 0
 		};
@@ -81,7 +174,13 @@ window.onload = function() {
 				this.dy *= -1;
 			}
 			if(this.x + this.size > W) {
-				//blue scores
+				if(gameMode === 1) {
+					//blue scores
+					blueScore++;
+				} else if(gameMode === 2) {
+					//computer scores
+					computerScore++;
+				}
 				this.dx = 4;
 				this.dy = 0;
 				this.x = 0;
@@ -89,6 +188,9 @@ window.onload = function() {
 			}
 			if(this.x < 0) {
 				//red scores
+				if(gameMode === 1 || gameMode === 2){
+					redScore++;
+				}
 				this.dx = -4;
 				this.dy = 0;
 				this.x = W - this.size;
@@ -100,39 +202,118 @@ window.onload = function() {
 			ctx.fillStyle = "#FFF";
 			ctx.fillRect(this.x, this.y, this.size, this.size);
 		},
+		reset: function() {
+			this.x = (W - ball.size)/2;
+			this.y = (H - ball.size)/2;
+			this.dy = 0;
+			this.dx = 4;
+		},
 		playerCollision: function() {
-			if(this.dx < 0) {
-				//checks for blue collision
-				if(this.x < player.blue.x + player.blue.w) {
-					if(this.y + this.size > player.blue.y && this.y < player.blue.y + player.blue.h) {
-						this.x = player.blue.x + player.blue.w;
-						this.dx *= -1;
-						if(player.blue.dy !== 0) {							
-							this.dy += player.blue.dy * -1;
+			if(gameMode === 1) {
+				if(this.dx < 0) {
+					//checks for blue collision
+					if(this.x < player.blue.x + player.blue.w) {
+						if(this.y + this.size > player.blue.y && this.y < player.blue.y + player.blue.h) {
+							this.x = player.blue.x + player.blue.w;
+							this.dx *= -1;
+							if(player.blue.dy !== 0) {							
+								this.dy = player.blue.dy * -1;
+							}
+							if(this.dx < 12) {
+								this.dx += 0.5;
+							}
 						}
-						if(this.dx < 12) {
-							this.dx += 0.5;
+					}
+				} else {
+					//checks for red collision
+					if(this.x + this.size > player.red.x) {
+						if(this.y + this.size > player.red.y && this.y < player.red.y + player.red.h) {
+							this.x = player.red.x - this.size;
+							this.dx *= -1;
+							if(player.red.dy !== 0) {
+								this.dy = player.red.dy * -1;
+							}
+							if(this.dx > -12){
+								this.dx -= 0.5;
+							}
 						}
 					}
 				}
-			} else {
-				//checks for red collision
-				if(this.x + this.size > player.red.x) {
-					if(this.y + this.size > player.red.y && this.y < player.red.y + player.red.h) {
-						this.x = player.red.x - this.size;
-						this.dx *= -1;
-						if(player.red.dy !== 0) {
-							this.dy += player.red.dy * -1;
+			} else if(gameMode === 2) {
+				console.log('colisao gamemode 2');
+				if(this.dx < 0) {
+					//checks for computer collision
+					if(this.x < computer.x + computer.w) {
+						if(this.y + this.size > computer.y && this.y < computer.y + computer.h) {
+							this.x = computer.x + computer.w;
+							this.dx *= -1;
+							if(computer.dy !== 0) {							
+								this.dy = computer.dy * -1;
+							}
+							if(this.dx < 12) {
+								this.dx += 0.5;
+							}
 						}
-						if(this.dx > -12){
-							this.dx -= 0.5;
+					}
+				} else {
+					//checks for red collision
+					if(this.x + this.size > player.red.x) {
+						if(this.y + this.size > player.red.y && this.y < player.red.y + player.red.h) {
+							this.x = player.red.x - this.size;
+							this.dx *= -1;
+							if(player.red.dy !== 0) {
+								this.dy = player.red.dy * -1;
+							}
+							if(this.dx > -12){
+								this.dx -= 0.5;
+							}
 						}
 					}
 				}
 			}
-			console.log(ball.dx);
 		}
-	}
+	};
+
+	var computer = {
+		x: 20,
+		y: H/2 - 50,
+		speed: 8,
+		dy: 0,
+		w: 20,
+		h: 100,
+		color: "#FFF",
+		update: function(){
+			console.log(ball.y);
+			if(ball.y < this.y) {
+				if(ball.dy !== 0) {
+					this.y += ball.dy*0.8;
+				} else {
+					this.y -= this.speed;
+				}
+			}
+			if(ball.y + ball.size > this.y + this.h) {
+				if(ball.dy !== 0){
+					this.y += ball.dy*0.8;
+				} else {
+					this.y += this.speed;
+				}
+			}
+			if(this.y < 0) {
+				this.y = 0;
+			}
+			if(this.y + this.h > H) {
+				this.y = H - this.h;
+			}
+		},
+		draw: function(){
+			ctx.fillStyle = this.color;
+			ctx.fillRect(this.x, this.y, this.w, this.h);
+		},
+		reset: function() {
+			this.x = 20;
+			this.y = H/2 - 50;
+		}
+	};
 
 	function init() {
 		ball.x = (W - ball.size)/2;
@@ -148,35 +329,174 @@ window.onload = function() {
 	}
 
 	function update() {
+		if(gameMode === 1){
+			updatePvP();
+		} else if(gameMode === 2) {
+			updatePvC();
+		} else if(gameMode === 3) {
+			updateCoOp();
+		} else {
+			updatePvW();
+		}
+	}
+
+	function updatePvP() {
 		player.blue.update();
 		player.red.update();
 		ball.update();
 	}
 
+	function updatePvC() {
+		computer.update();
+		player.red.update();
+		ball.update();
+	}
+
+	function updateCoOp() {
+		player.blue.update();
+		player.red.update();
+		ball.update();
+	}
+
+	function updatePvW() {
+		player.red.update();
+		ball.update();
+	}
+
 	function draw() {
+		//0: menu; 1: player vs player; 2: player vs computer; 3: co-op 4: Player vs Wall;
+		drawScenario();
+		if(gameMode === 0) {
+			drawMenu();
+		} else if(gameMode === 1) {
+			drawPvP();
+		} else if(gameMode === 2) {
+			drawPvC();
+		} else if(gameMode === 3) {
+			drawCoOp();
+		} else {
+			drawPvW();
+		}
+	}
+
+	function drawScenario() {
 		ctx.fillStyle = "#000";
 		ctx.fillRect(0,0,W,H);
+	}
+
+	function drawMenu() {
+		menu.draw();
+	}
+
+	function drawPvP() {
+		ctx.font="40px Helvetica";
+		ctx.fillStyle = player.blue.color;
+		ctx.fillText(blueScore, player.blue.x, 40);
+		ctx.fillStyle = player.red.color;
+		ctx.fillText(redScore, player.red.x, 40);
 		player.blue.draw();
 		player.red.draw();
 		ball.draw();
 	}
 
+	function drawPvC() {
+		ctx.font="40px Helvetica";
+		ctx.fillStyle = computer.color;
+		ctx.fillText(computerScore, computer.x, 40);
+		ctx.fillStyle = player.red.color;
+		ctx.fillText(redScore, player.red.x, 40);
+		player.red.draw();
+		computer.draw();
+		ball.draw();
+	}
+
+	function drawCoOp() {
+		player.blue.draw();
+		player.red.draw();
+		ball.draw();	
+	}
+
+	function drawPvW() {
+		player.red.draw();
+		wall.draw();
+		ball.draw();
+	}
+
+	function startPvP() {
+		gameMode = 1;
+		player.blue = new Player(20, "#00F");
+		player.red = new Player(W - 40, "#F00");
+		ball.reset();
+	}
+
+	function startPvC() {
+		gameMode = 2;
+		player.red = new Player(W - 40, "#F00");
+		computer.reset();
+		ball.reset();
+	}
+
+	function startCoOp() {
+		gameMode = 3;
+		console.log('test');
+	}
+
+	function startPvW() {
+		gameMode = 4;
+		console.log('test');
+	}
+
 	function keyPressed(key) {
+		console.log(key.which);
+		if(gameMode === 0) {
+			menuKeyPressed(key);
+		}
+		if(gameMode === 1) {
+			pvpKeyPressed(key);
+		}
+		if(gameMode === 2) {
+			pvcKeyPressed(key);
+		}
+
+	}
+
+	function menuKeyPressed(key) {
+		if(key.which === wKey || key.which === upArrow) {
+			menu.selectUp();
+		} else if(key.which === sKey || key.which === downArrow) {
+			menu.selectDown();
+		} else if(key.which === enterKey) {
+			menu.chooseGameMode();
+		}
+	}
+
+	function pvpKeyPressed(key) {
 		if(key.which === wKey) {
 			//sobe player azul
 			player.blue.goUp();
-		}
-		if(key.which === sKey) {
+		} else if(key.which === sKey) {
 			//desce player azul
 			player.blue.goDown();
+		} else if(key.which === upArrow) {
+			//sobe player vermelho
+			player.red.goUp();
+		} else if(key.which === downArrow) {
+			//desce player vermelho
+			player.red.goDown();
+		} else if(key.which === escKey) {
+			gameMode = 0;
 		}
+	}
+
+	function pvcKeyPressed(key) {
 		if(key.which === upArrow) {
 			//sobe player vermelho
 			player.red.goUp();
-		}	
-		if(key.which === downArrow) {
+		} else if(key.which === downArrow) {
 			//desce player vermelho
 			player.red.goDown();
+		} else if(key.which === escKey) {
+			gameMode = 0;
 		}
 	}
 
