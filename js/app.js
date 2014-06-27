dcb4644c3f4446bfa310ace7032e860f4e3cac59
Lucas Vieira <lucas@lucas-ubuntu.(none)>
2014-06-27 03:31:09 -0300

    upando 15-puzzle

--- js/app.js
+++ js/app.js
@@ -1,38 +1,38 @@
 var app = angular.module('app', ['ngRoute']);
 
- // Note: Providers can only be injected into config functions. Thus you could not inject $routeProvider into PhoneListCtrl. 
-app.config(['$routeProvider',
-  function($routeProvider) {
+app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
       when('/home', { templateUrl: 'partials/home.html', controller: 'HomeController'}).
       when('/projects', {templateUrl: 'partials/projects.html', controller: 'ProjectsController'}).
-      when('/pong', {templateUrl: 'partials/pong/pong.html', controller: 'PongController'}).
+      when('/pong', {templateUrl: 'partials/pong/pong.html', controller: 'ProjectsController'}).
+      when('/15-puzzle' {templateUrl: 'partials/15-puzzle/15-puzzle.html', controller: 'ProjectsController'}).
       otherwise({
         redirectTo: '/home'
       });
   }
 ]);
 
-app.controller('MainController', function($scope){
-	this.nav = 1;
+app.factory('projectsFactory', function(){
+  var factory = {};
+  factory.test = function() {
+    console.log('factory test');
+  };
+  return factory;
+});
 
-    this.setNav = function(newValue){
-      this.nav = newValue;
-    };
-
-    this.isSet = function(navName){
-      return this.nav === navName;
-    };
+app.controller('NavController', function($scope, $location){
+	$scope.isActive = function(path) {
+    if($location.path() === path) {
+      return true;
+    }
+    return false;
+  }
 })
 
 app.controller('HomeController', function(){
 	
 });
 
-app.controller('ProjectsController',function(){
-
-});
-
-app.controller('PongController',function(){
-	console.log('test');
+app.controller('ProjectsController',function(projectsFactory){
+  projectsFactory.test();
 });
